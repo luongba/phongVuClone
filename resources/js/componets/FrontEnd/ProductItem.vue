319424c505a45d2978fe56e2c5c162456f897967
luongba <a2k10lnc@gmail.com>
2022-10-31 17:33:18 +0700

    Thêm ảnh, icon

---
+++ resources/js/componets/FrontEnd/ProductItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-white border h-full px-4 py-8">
+        <div class="flex flex-center">
+            <img src="https://lh3.googleusercontent.com/ms2dvVvV-F1fCsnZOhLcFKWHiDxipdSKBhHPW4BrmASMFJB2AgfOico4EvjasPUfbCyhNmbQiTS4e1oIY9YDwIcJEmzc9Bro=w230-rw" alt="">
+        </div>
+        <div class="">
+            <p class="text-[13px] text-gray-600 font-semibold">Chuột không dây Akko Smart 1 Sailor Venus</p>
+            <div class="flex justify-between">
+                <p class="main-color text-[15px] font-bold">125.000 đ</p>
+<!--                <img class="w-[24px] h-[24px]" src="./images/icons/free-ship.svg" alt="">-->
+            </div>
+
+            <p class="text-orange-400 text-[13px] font-semibold ">Còn 4 sản phẩm</p>
+            <span class="text-gray-400 text-[13px] line-through">249.000 đ</span>
+            <span class="main-color text-[12px] font-semibold ml-2">-10%</span>
+        </div>
+    </div>
+</template>
