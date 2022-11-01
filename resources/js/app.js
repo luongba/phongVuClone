1ddefa9179a50d472ac4f51aa5015f80e6685282
luongba <a2k10lnc@gmail.com>
2022-11-01 17:32:16 +0700

    them file anh

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import {createApp} from 'vue'
 import Header from './componets/FrontEnd/Header'
 import HeroSlider from './componets/FrontEnd/HeroSlider'
 import ProductItem from "./componets/FrontEnd/ProductItem"
+import ProductCategory from "./componets/FrontEnd/ProductCategory"
+import BannerList from "./componets/FrontEnd/BannerList"
+import BrandList from "./componets/FrontEnd/BrandList"
+import ProductList from "./componets/FrontEnd/ProductList"
 import mdiVue from 'mdi-vue/v3'
 import * as mdijs from '@mdi/js'
 const app = createApp({})
@@ -11,6 +15,12 @@ const app = createApp({})
 app.component('HeaderApp', Header)
 app.component('HeroSlider', HeroSlider)
 app.component('ProductItem', ProductItem)
+app.component('ProductCategory', ProductCategory)
+app.component('BannerList', BannerList)
+app.component('BrandList', BrandList)
+app.component('ProductList', ProductList)
+
+
 
 app.use(mdiVue, {
     icons: mdijs
